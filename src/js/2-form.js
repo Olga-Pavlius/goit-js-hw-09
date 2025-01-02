6d3dd1210ba2f84c67f69456c7ed66841dfe27c2
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-01-02 21:58:00 +0200

    changed 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,4 @@
+const feedbackFormEl = document.querySelector('.feedback-form');
 const inputForm = document.querySelector('input');
 inputForm.classList.add('input-form-email');
 const textareaForm = document.querySelector('textarea');
@@ -5,69 +6,57 @@ textareaForm.classList.add('textarea-form-message');
 const buttonForm = document.querySelector('button');
 buttonForm.classList.add('btn-form');
 
-const feedbackFormEl = document.querySelector('.feedback-form');
-
-const formData = {
+let formData = {
   email: '',
   message: '',
 };
-form.addEventListener(
-  'focus',
-  event => {
-    if (event.target.tagName === 'INPUT') {
-      event.target.placeholder = event.target.type;
-    }
-    if (event.target.tagName === 'TEXTAREA') {
-      event.target.placeholder = event.target.type;
-    }
-  },
-  true
-);
-form.addEventListener(
-  'blur',
-  event => {
-    if (event.target.tagName === 'INPUT') {
-      event.target.placeholder = '';
-    }
-    if (event.target.tagName === 'TEXTAREA') {
-      event.target.placeholder = '';
-    }
-  },
-  true
-);
-form.addEventListener('input', () => {
-  formData.email = input.value;
-  formData.message = textArea.value;
 
-  try {
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  } catch (error) {
-    console.log(error);
-  }
-});
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  if (!formData.email || !formData.message) {
-    alert('Fill please all fields');
-  }
-  console.log(formData);
-  input.value = '';
-  textArea.value = '';
-  formData.email = '';
-  formData.message = '';
-  localStorage.clear();
-});
-const getValue = () => {
-  try {
-    const formLS = JSON.parse(localStorage.getItem('feedback-form-state'));
-    if (formLS.email !== null || formLS.message !== null) {
-      input.value = formLS.email;
-      textArea.value = formLS.message;
-      formData.email = formLS.email;
-      formData.message = formLS.message;
+const fillFormFields = event => {
+  try { 
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (localStorage.length === null) {
+      return;
     }
-  } catch (error) {
-    console.log(error);
+
+    formData = formDataFromLS;
+    for (const key in formDataFromLS) {
+      feedbackFormEl.elements[key].value = formDataFromLS[key];
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
-getValue();
+
+fillFormFields();
+
+const onFormFieldChange = event => {
+  const { target: formFieldEl } = event;
+
+  const fieldValue = formFieldEl.value;
+  const fieldName = formFieldEl.name;
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+  
+  if (!(formData.email && formData.email.trim()) || !(formData.message && formData.message.trim())) {
+    alert('Fill please all fields');
+    return;
+   }
+
+console.log(formData);
+
+formData = {};
+const formEl = event.currentTarget;
+
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+
+feedbackFormEl.addEventListener('change', onFormFieldChange);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
